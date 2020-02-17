c078a1fa089eb5b0dc65851e668bf35f428d784f
hu <hujiabing2016@sina.com>
2020-02-18 06:57:15 +0800

    models

--- typeidea/settings/develop.py
+++ typeidea/settings/develop.py
@@ -4,6 +4,7 @@ DEBUG = True
 DATABASES = {
     'default':{
         'ENGINE':'django.db.backends.sqlite3',
+#         'ENGINE':'django.db.backends.mysql',
         'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
         }
     }
